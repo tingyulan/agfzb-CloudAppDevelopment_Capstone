70fcc011e703c06aca4e8e7a0aa2d481165e8f26
Lan, Ting-Yu <tingyu.lan@gatech.edu>
2023-10-08 20:49:34 -0400

    signin

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,8 +18,10 @@ urlpatterns = [
     # path for registration
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
